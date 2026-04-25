e708e4fb270ae154b5f2ef2a99197dff9ec578d8
D1533 <davidmanresa94@gmail.com>
2026-04-25 15:53:55 +0200

    fixes

--- tests/test_svm.py
+++ tests/test_svm.py
@@ -36,7 +36,7 @@ def main():
     model.fit(X_train, y_train, lr=1e-3, epochs=500, C=1.0)
 
     # --- Plot ---
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(6,6))
     fig.subplots_adjust(top=0.8)
     x_vals = np.linspace(X[:, 0].min() - 1, X[:, 0].max() + 1, 200)
     def update(epoch):
